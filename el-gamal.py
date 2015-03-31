a37c45fdd621e46fa49c572962f760fdec329af6
JS <jean-serge.monbailly@etudiant.univ-lille1.fr>
2015-03-31 19:32:41 +0200

    Fin du travail sur El-Gamal

--- el-gamal.py
+++ el-gamal.py
@@ -13,7 +13,28 @@ from client import *
 URL = 'http://pac.bouillaguet.info/TP3/ElGamal-encryption/'
 server = Server(URL)
 
-S = 1 # Valeur utilisée pour le chiffrement
+x = 1 # Valeur utilisée pour le chiffrement
+
+def xgcd(a,b):
+    """
+    Calcul de XGCD(a, b) récupéré sur internet
+    """
+    prevx, x = 1, 0;  prevy, y = 0, 1
+    while b:
+        q, r = divmod(a,b)
+        x, prevx = prevx - q*x, x
+        y, prevy = prevy - q*y, y
+        a, b = b, r
+    return a, prevx, prevy
+                                                
+
+def inverse_mod(x, y):
+    """
+    Calcul de l'inverse modulaire de x par rapport à y.
+    Revient à calculer le XGCD(x, y)
+    """
+    a,b,c = xgcd(x, y)
+    return b
 
 # ----------------------------------------------------------------------------
 # Récupération des champs p et q
@@ -22,7 +43,7 @@ response = server.query('parameters/monbailly') # contient p et g
 
 g = response['g']
 p = response['p']
-h = pow(g, S, p)
+h = pow(g, x, p)
 
 
 # ----------------------------------------------------------------------------
@@ -34,17 +55,20 @@ response = server.query('challenge/monbailly', dic)
 c1 = response['ciphertext'][0]
 c2 = response['ciphertext'][1]
 
+
 # ----------------------------------------------------------------------------
 # Déchiffrement du message  
 # -------------------------
-invC1 = pow(c1, p-2)
+c1x = pow(c1, x, p)
+inv = inverse_mod(c1x, p)
 
-cipher = c2 * invC1
+cipher = (c2 * inv) % p
 print(cipher)
+
 
 # ----------------------------------------------------------------------------
 # Envoi du message
 # -------------------------
 dic = {'plaintext':cipher}
 response = server.query('validate/monbailly', dic)
-print(response)
+print(response) # status : OK
